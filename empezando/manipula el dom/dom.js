8ae5ca778cd898d7d620beb96986b52b11a8f6f3
Anonymous24 <121126125+DarkMoonG@users.noreply.github.com>
2022-12-27 09:32:05 -0400

    Add files via upload

---
+++ empezando/manipula el dom/dom.js
@@ -0,0 +1,57 @@
+/* document de asignacion para la similitud en consola */
+let elemento=window;
+
+elemento=document;
+console.log("elemento");
+/*buscar una etiqueta en el html*/
+let elemen=document.querySelector('h2');
+console.log(elemen);
+/* ver detalles de la hoja de estilos*/ 
+let stye=document.styleSheets;
+console.log(stye);
+/*document.links, forms, images */ 
+/*document.queryselector() , muestra clases o id
+ solo presenta lo primero que encuentre, queryselectorALLL para todo */
+
+ /* crear y agregar 
+    para esto debemos ramificar los elementos del documento
+    document.createElement */
+    const father =document.querySelector()
+    /*con esta creamos un elemento pero no forma parte del padre o del hijo solo se crea
+    un nodo*/
+    const nodo= document.createElement('h2')
+    /*con appendchild agregamos un hijo al padre*/ 
+    father.appendChild(nodo)
+    /*agregar un contenido a lo agregado document.createtextnode para texto*/
+    const texto=document.createTextNode("caracter especial")
+    /*formar parte el texto en nodo*/
+    nodo.appendChild(texto) /*los appenchild se agregan al final del padre ws-black w3-center
+    insertbefore se coloca el nodo en el lugar conveniente insertadjacentelement*/
+    nodo.insertadjacentelement('beforebegin',father)
+    /*eliminar y reemplazar removechild  replacechild */
+    const nodo_eli=$0
+    nodo_eli
+    const padre = $0
+    padre
+    padre.removeChild(nodo_eli)
+    /*remplazar y eliminar*/ 
+    const referencia = document.querySelector('div.tipos')
+    referencia
+    referencia.remove()
+    const nuevo_nudo=document.createElement('h1')
+    nuevo_nodo.textcontent = "nuevo texto"
+    const reemplazar = $0
+    reemplazar
+    nuevo_nodo
+    padre.replaceChild(nuevo_nodo, reemplazar)
+    /*colecciones del dom los nodelist no son arrays*/ 
+    const array =[...lista]
+    array
+    lista
+    lista.map
+    array.map
+    const array2 =array.from(document.querySelectorAll('img'))
+    array2
+   const array3=[...colletion]
+   array3
+
